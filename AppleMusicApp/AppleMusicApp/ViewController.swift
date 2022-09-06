58ab18a6123ccc467d3b5d00f2a632ab5260dd20
yexjin <tnska0421@naver.com>
2022-09-06 15:37:05 +0900

    [MusicApp] add music resources

---
+++ AppleMusicApp/AppleMusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMusicApp
+//
+//  Created by 오예진 on 2022/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
